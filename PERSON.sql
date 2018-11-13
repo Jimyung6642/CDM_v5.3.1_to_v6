e9197f08aba5d4b0865febd8cb854f007641351d
Jimyung6642 <cory66421@gmail.com>
2018-11-13 09:55:44 +0900

    Create PERSON.sql

---
+++ PERSON.sql
@@ -0,0 +1,49 @@
+select	
+	person_id,
+	gender_concept_id,
+	year_of_birth,
+	month_of_birth,
+	day_of_birth,
+	convert(datetime, year_of_birth) as birth_datetime,
+	convert(datetime, NULL) as death_datetime,
+	race_concept_id,
+	ethnicity_concept_id,
+	location_id,
+	provider_id,
+	care_site_id,
+	person_source_value,
+	gender_source_value,
+	gender_source_concept_id,
+	race_source_value,
+	race_source_concept_id,
+	ethnicity_source_value,
+	ethnicity_source_concept_id
+
+--into DatabaseSchemaCDMVersion6.person
+from DatabaseSchemaCDMVersion5.PERSON 
+where person_id not in (select person_id from DatabaseSchemaCDMVersion5.DEATH)
+
+--Insert into DatabaseSchemaCDMVersion6.dbo.person
+select
+	a.person_id,
+	gender_concept_id,
+	year_of_birth,
+	month_of_birth,
+	day_of_birth,
+	convert(datetime, year_of_birth) as birth_datetime,
+	convert(datetime, death_date) as death_datetime,
+	race_concept_id,
+	ethnicity_concept_id,
+	location_id,
+	provider_id,
+	care_site_id,
+	person_source_value,
+	gender_source_value,
+	gender_source_concept_id,
+	race_source_value,
+	race_source_concept_id,
+	ethnicity_source_value,
+	ethnicity_source_concept_id
+
+from DatabaseSchemaCDMVersion5.PERSON a, DatabaseSchemaCDMVersion5.DEATH b
+where a.person_id=b.person_id
