79ea1f3a422406001b2f8eba28d51d93c6947d40
Jimyung6642 <cory66421@gmail.com>
2018-11-13 10:07:21 +0900

    Create DOSE_ERA.sql

---
+++ DOSE_ERA.sql
@@ -0,0 +1,11 @@
+select
+	dose_era_id,
+	person_id,
+	drug_concept_id,
+	unit_concept_id,
+	dose_value,
+	convert(datetime, dose_era_start_date) as dose_era_start_datetime,
+	convert(datetime, dose_era_end_date) as dose_era_end_datetime
+
+into DatabaseSchemaCDMVersion6.dose_era
+from DatabaseSchemaCDMVersion5.DOSE_ERA
