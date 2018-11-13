3fdd447f334c12bee69477cb100836f294d8664c
Jimyung6642 <cory66421@gmail.com>
2018-11-13 10:04:45 +0900

    Create COST.sql

---
+++ COST.sql
@@ -0,0 +1,107 @@
+-- Insert data from Device_exposure
+select 
+	cost_id,
+	b.person_id,
+	cost_event_id,
+	case when cost_domain_id='device' then device_concept_id 
+		end as cost_event_field_concept_id,
+	NULL as cost_concept_id,			
+	cost_type_concept_id,
+	NULL as cost_source_concept_id,			
+	NULL as cost_source_value,
+	currency_concept_id,
+	total_cost as cost,
+	case when cost_domain_id='device' then device_exposure_start_date 
+		 end as incurred_date,
+	NULL as billed_date,
+	NULL as paid_date,
+	revenue_code_concept_id,
+	drg_concept_id,
+	a.payer_plan_period_id
+
+into DatabaseSchemaCDMVersion6.dbo.cost
+from DatabaseSchemaCDMVersion5.COST a, DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, DatabaseSchemaCDMVersion5.DEVICE_EXPOSURE c
+where a.payer_plan_period_id=b.payer_plan_period_id
+	and b.person_id=c.person_id
+	and cost_domain_id='device'
+
+-- Insert data from Drug_exposure
+select 
+	cost_id,
+	b.person_id,
+	cost_event_id,
+	case when cost_domain_id='drug' then drug_concept_id 
+		end as cost_event_field_concept_id,
+	NULL as cost_concept_id,			
+	cost_type_concept_id,
+	NULL as cost_source_concept_id,			
+	NULL as cost_source_value,
+	currency_concept_id,
+	total_cost as cost,
+	case when cost_domain_id='drug' then drug_exposure_start_date 
+		 end as incurred_date,
+	NULL as billed_date,
+	NULL as paid_date,
+	revenue_code_concept_id,
+	drg_concept_id,
+	a.payer_plan_period_id
+
+into DatabaseSchemaCDMVersion6.dbo.cost
+from DatabaseSchemaCDMVersion5.COST a, DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, DatabaseSchemaCDMVersion5.DRUG_EXPOSURE c
+where a.payer_plan_period_id=b.payer_plan_period_id
+	and b.person_id=c.person_id
+	and cost_domain_id='drug'
+
+-- Insert data from Procedure_occurrence
+select 
+	cost_id,
+	b.person_id,
+	cost_event_id,
+	case when cost_domain_id='procedure' then procedure_concept_id
+		end as cost_event_field_concept_id,
+	NULL as cost_concept_id,
+	cost_type_concept_id,
+	NULL as cost_source_concept_id,			
+	NULL as cost_source_value,
+	currency_concept_id,
+	total_cost as cost,
+	case when cost_domain_id='procedure' then procedure_date
+		 end as incurred_date,
+	NULL as billed_date,
+	NULL as paid_date,
+	revenue_code_concept_id,
+	drg_concept_id,
+	a.payer_plan_period_id
+
+into DatabaseSchemaCDMVersion6.dbo.cost
+from DatabaseSchemaCDMVersion5.COST a, DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, DatabaseSchemaCDMVersion5.PROCEDURE_OCCURRENCE c
+where a.payer_plan_period_id=b.payer_plan_period_id
+	and b.person_id=c.person_id
+	and cost_domain_id='procedure'
+
+-- Insert data from Visit_occurrence
+select 
+	cost_id,
+	b.person_id,
+	cost_event_id,
+	case when cost_domain_id='visit' then visit_concept_id 
+		end as cost_event_field_concept_id,	-- 이건 뭐하는 변수인걸까..
+	NULL as cost_concept_id,			-- concept table 에 있는 cost concept_id 들 넣어줘야함
+	cost_type_concept_id,
+	NULL as cost_source_concept_id,			
+	NULL as cost_source_value,
+	currency_concept_id,
+	total_cost as cost,
+	case when cost_domain_id='visit' then visit_start_date
+		 end as incurred_date,
+	NULL as billed_date,
+	NULL as paid_date,
+	revenue_code_concept_id,
+	drg_concept_id,
+	a.payer_plan_period_id
+
+into DatabaseSchemaCDMVersion6.cost
+from DatabaseSchemaCDMVersion5.COST a, DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, DatabaseSchemaCDMVersion5.VISIT_OCCURRENCE c
+where a.payer_plan_period_id=b.payer_plan_period_id
+	and b.person_id=c.person_id
+	and cost_domain_id='visit'
