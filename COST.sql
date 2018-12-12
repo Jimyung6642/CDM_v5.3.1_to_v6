7942f80d2184ab194099a1b8885ade36f2630610
Jimyung6642 <cory66421@gmail.com>
2018-12-12 09:59:19 +0900

    Update COST.sql

--- COST.sql
+++ COST.sql
@@ -19,15 +19,15 @@ select
 	drg_concept_id,
 	a.payer_plan_period_id
 
-into DatabaseSchemaCDMVersion6.dbo.cost
-from DatabaseSchemaCDMVersion5.COST a, DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, DatabaseSchemaCDMVersion5.DEVICE_EXPOSURE c
+into @DatabaseSchemaCDMVersion6.dbo.cost
+from @DatabaseSchemaCDMVersion5.COST a, @DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, @DatabaseSchemaCDMVersion5.DEVICE_EXPOSURE c
 where a.payer_plan_period_id=b.payer_plan_period_id
 	and b.person_id=c.person_id
 	and a.cost_id=c.device_exposure_id
 	and cost_domain_id='device'
 
 --Insert data from Drug_exposure
-Insert into DatabaseSchemaCDMVersion6.dbo.cost
+Insert into @DatabaseSchemaCDMVersion6.dbo.cost
 select 
 	cost_id,
 	b.person_id,
@@ -48,14 +48,14 @@ select
 	drg_concept_id,
 	a.payer_plan_period_id
 
-from DatabaseSchemaCDMVersion5.COST a, DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, DatabaseSchemaCDMVersion5.DRUG_EXPOSURE c
+from @DatabaseSchemaCDMVersion5.COST a, @DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, @DatabaseSchemaCDMVersion5.DRUG_EXPOSURE c
 where a.payer_plan_period_id=b.payer_plan_period_id
 	and b.person_id=c.person_id
 	and a.cost_id=c.drug_exposure_id
 	and cost_domain_id='drug'
 
 --Insert data from Procedure_occurrence
-Insert into DatabaseSchemaCDMVersion6.dbo.cost
+Insert into @DatabaseSchemaCDMVersion6.dbo.cost
 select 
 	cost_id,
 	b.person_id,
@@ -76,14 +76,14 @@ select
 	drg_concept_id,
 	a.payer_plan_period_id
 
-from DatabaseSchemaCDMVersion5.COST a, DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, DatabaseSchemaCDMVersion5.PROCEDURE_OCCURRENCE c
+from @DatabaseSchemaCDMVersion5.COST a, @DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, @DatabaseSchemaCDMVersion5.PROCEDURE_OCCURRENCE c
 where a.payer_plan_period_id=b.payer_plan_period_id
 	and b.person_id=c.person_id
 	and a.cost_id=c.procedure_occurrence_id
 	and cost_domain_id='procedure'
 
 --Insert data from Visit_occurrence
-Insert into DatabaseSchemaCDMVersion6.cost
+Insert into @DatabaseSchemaCDMVersion6.cost
 select 
 	cost_id,
 	b.person_id,
@@ -104,7 +104,7 @@ select
 	drg_concept_id,
 	a.payer_plan_period_id
 
-from DatabaseSchemaCDMVersion5.COST a, DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, DatabaseSchemaCDMVersion5.VISIT_OCCURRENCE c
+from @DatabaseSchemaCDMVersion5.COST a, @DatabaseSchemaCDMVersion5.PAYER_PLAN_PERIOD b, @DatabaseSchemaCDMVersion5.VISIT_OCCURRENCE c
 where a.payer_plan_period_id=b.payer_plan_period_id
 	and b.person_id=c.person_id
 	and a.cost_id=c.visit_occurrence_id
